8136e67bb7e471ea618c988d321921adadc5307a
Jacopo Corbetta <jacopo.corbetta@gmail.com>
2014-08-17 01:20:27 -0700

    Create one game at a time via system()

--- vmcreator/spawner.py
+++ vmcreator/spawner.py
@@ -13,7 +13,7 @@ while True:
         games = requests.get('http://ictf.cs.ucsb.edu/framework/ctf/pending?secret='+secrets.API_SECRET).json()
         if 'ctf_hash' in games:
             print "Spawning the creator for", games['ctf_hash']
-            os.spawnl("./create_vms.py", games['ctf_hash'])
+            os.system("./create_vms.py "+games['ctf_hash'])
     except:
         print "Exception while getting the pending CTF or spawning its creation!"
     time.sleep(30)

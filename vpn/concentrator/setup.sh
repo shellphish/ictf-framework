566d1afad327c227d659d6f2d3e6757a5e5f8521
cao <cao@cao.vc>
2014-08-18 14:29:03 -0700

    vpn: add routes to concentrator setup

--- vpn/concentrator/setup.sh
+++ vpn/concentrator/setup.sh
@@ -12,3 +12,7 @@ sysctl -w net.ipv4.ip_forward=1
 tar -xf concentrator.tgz -C /etc/openvpn/
 
 service openvpn restart ictf-in-a-box
+
+for ${TEAM} in ${TEAMS}; do
+    route add 10.7.${TEAM}.2 dev eth1
+done

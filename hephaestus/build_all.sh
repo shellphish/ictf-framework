30b0b08f968b05b18676438070dff33ed6c378d0
Sebastiano Mariani <mariani.sebastiano@gmail.com>
2020-04-22 19:55:37 -0700

    Working on new version of the building system
    Signed-off-by: Sebastiano Mariani <mariani.sebastiano@gmail.com>

---
+++ hephaestus/build_all.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# docker-compose does not handle dependency at building time so we need to use
+# this workaround in order to use parallel build without breaking ther dependecy
+# graph
+docker-compose -f ./docker-compose-base.yml build heat
+docker-compose -f ./docker-compose-base.yml build melt
+
+docker-compose -f ./docker-compose-cast.yml build --parallel

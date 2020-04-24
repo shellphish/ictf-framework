63c93ce631687c01b82e4cf49f2bbfec64c61299
Sebastiano Mariani <mariani.sebastiano@gmail.com>
2020-04-24 12:25:19 -0700

    Finish database porting to new buildingh setup
    Signed-off-by: Sebastiano Mariani <mariani.sebastiano@gmail.com>

---
+++ database/start.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+service mysql restart
+
+service nginx restart
+
+ICTF_DATABASE_SETTINGS=/opt/ictf/settings/database-api.py  /usr/bin/uwsgi -c uwsgi.ini
+
+/opt/ictf/venv/database/bin/python ictf-db-export-s3.py

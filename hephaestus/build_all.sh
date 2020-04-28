2012830f1549f093484e710843ee1df995602d35
Sebastiano Mariani <mariani.sebastiano@gmail.com>
2020-04-28 15:48:42 -0700

    Removed useless building stages
    Signed-off-by: Sebastiano Mariani <mariani.sebastiano@gmail.com>

--- hephaestus/build_all.sh
+++ hephaestus/build_all.sh
@@ -3,9 +3,6 @@
 # docker-compose does not handle dependency at building time so we need to use
 # this workaround in order to use parallel build without breaking ther dependecy
 # graph
-docker-compose -f ./docker-compose-base.yml build heat
-docker-compose -f ./docker-compose-base.yml build melt
+docker-compose -f ./docker-compose-base.yml build
 
-docker-compose -f ./docker-compose-cast.yml build --parallel
-
-docker-compose -f ./docker-compose-harden.yml build --parallel
+docker-compose -f ./docker-compose-infra.yml build --parallel

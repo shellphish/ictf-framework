3e8e23415d4b7dfe3affe62c0c22f3a34ad3ccdb
Sebastiano Mariani <mariani.sebastiano@gmail.com>
2020-04-29 10:35:24 -0700

    Teaminterface works live locally now
    Signed-off-by: Sebastiano Mariani <mariani.sebastiano@gmail.com>

--- teaminterface/start.sh
+++ teaminterface/start.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-service nginx restart
+service nginx start
 # TODO: Investigate what's the impact of the removed performance tweaks (Listen queue size is greater than the system max net.core.somaxconn (128))
 # uwsgi -s /tmp/ictf-api.sock --chmod-socket --logto /tmp/uwsgi.log --module team_interface --callable app --enable-threads -z 6000 --master --listen 65535 --processes 10 --max-requests 655350 --die-on-term
 uwsgi -s /tmp/ictf-api.sock --chmod-socket --logto /tmp/uwsgi.log --module team_interface --callable app --enable-threads -z 6000 --master --processes 10 --die-on-term

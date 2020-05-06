83045f7da33fd6d606f130a632d053eb624aed60
Sebastiano Mariani <mariani.sebastiano@gmail.com>
2020-05-05 17:35:32 -0700

    Add new screapt to ease deployment
    Signed-off-by: Sebastiano Mariani <mariani.sebastiano@gmail.com>

--- database/provisioning/create_services.py
+++ database/provisioning/create_services.py
@@ -119,12 +119,10 @@ if __name__ == "__main__":
     db_api = sys.argv[1]  # passed by terraform
     database_api_secret_path = SECRETS_FOLDER + "database-api/secret"
 
-    services_folder_path = sys.argv[2]  # passed by terraform
-
     if not os.path.isfile(database_api_secret_path):
         raise Exception("Missing database secrets!")
 
     with open(database_api_secret_path, "r") as f:
         database_api_secret = f.read().rstrip()
 
-    create_all_services('http://{}'.format(db_api), database_api_secret, game_config['services'], services_folder_path)
+    create_all_services('http://{}'.format(db_api), database_api_secret, game_config['services'], game_config["service_metadata"]['host_dir'])

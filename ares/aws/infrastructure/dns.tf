f19163adc00b84436b360a9216ec22f234d1f40f
Sebastiano Mariani <mariani.sebastiano@gmail.com>
2020-05-20 18:09:11 -0700

    add remote logger setup
    Signed-off-by: Sebastiano Mariani <mariani.sebastiano@gmail.com>

--- ares/aws/infrastructure/dns.tf
+++ ares/aws/infrastructure/dns.tf
@@ -13,3 +13,11 @@ resource "aws_route53_record" "database" {
   ttl     = "43200" // 12h
   records = [aws_instance.database.private_ip]
 }
+
+resource "aws_route53_record" "logger" {
+  zone_id = aws_route53_zone.private.zone_id
+  name    = "logger.ictf"
+  type    = "A"
+  ttl     = "43200" // 12h
+  records = [aws_instance.logger.private_ip]
+}
--- ares/aws/infrastructure/logger.tf
+++ ares/aws/infrastructure/logger.tf
@@ -1,53 +1,47 @@
-# resource "aws_instance" "logger" {
-#     ami = data.aws_ami.ictf_base.id
-#     instance_type = var.logger_instance_type
-#     subnet_id = aws_subnet.master_and_db_range_subnet.id
-#     vpc_security_group_ids = [aws_security_group.master_subnet_secgrp.id]
-#     key_name = aws_key_pair.logger-key.key_name
+resource "aws_instance" "logger" {
+    ami = data.aws_ami.ictf_base.id
+    instance_type = var.logger_instance_type
+    subnet_id = aws_subnet.master_and_db_range_subnet.id
+    vpc_security_group_ids = [aws_security_group.master_subnet_secgrp.id]
+    key_name = aws_key_pair.logger-key.key_name
 
-#     tags = {
-#         Name = "logger"
-#         Type = "Infrastructure"
-#     }
+    tags = {
+        Name = "logger"
+        Type = "Infrastructure"
+    }
 
-#     root_block_device {
-#         volume_size = 100
-#     }
+    root_block_device {
+        volume_size = 100
+    }
 
-#     volume_tags = {
-#         Name = "logger-disk"
-#     }
+    volume_tags = {
+        Name = "logger-disk"
+    }
 
-#     connection {
-#         user = "hacker"
-#         private_key = file("./sshkeys/logger-key.key")
-#         host = self.public_ip
-#         agent = false
-#     }
+    connection {
+        user = local.ictf_user
+        private_key = file("./sshkeys/logger-key.key")
+        host = self.public_ip
+        agent = false
+    }
 
-#     provisioner "remote-exec" {
-#         inline = [
-#             "mkdir -p /opt/ictf/logger/provisioning"
-#         ]
-#     }
-    
+    # TODO: This ansible role shold be already inside the base ictf AMI
+    #       Remove this once you have a base image
+    provisioner "file" {
+        source = "../../common/ares_provisioning"
+        destination = "~/"
+    }
 
-#     provisioner "file" {
-#         source = "../../logger/provisioning/prometheus.yml.j2"
-#         destination = "/opt/ictf/logger/provisioning/prometheus.yml.j2"
-#     }
+    provisioner "file" {
+        source = "../../logger/provisioning/ares_provisioning/docker-compose.yml"
+        destination = "~/docker-compose.yml"
+    }
 
-#     provisioner "file" {
-#         source = "../../logger/provisioning/terraform_provisioning.yml"
-#         destination = "/opt/ictf/logger/provisioning/terraform_provisioning.yml"
-#     }
-
-#     provisioner "remote-exec" {
-#         inline = [
-#             "sudo pip install -q ansible",
-#             #"/usr/local/bin/ansible-playbook /opt/ictf/logger/provisioning/terraform_provisioning.yml --extra-vars SOURCES='{}'", 
-#             "echo 'hacker' | sudo sed -i '/^#PasswordAuthentication[[:space:]]yes/c\\PasswordAuthentication no' /etc/ssh/sshd_config",
-#             "sudo service ssh restart"
-#         ]
-#     }
-# }
+    provisioner "remote-exec" {
+        inline = [
+            local.logger_provision_with_ansible,
+            "sudo sysctl vm.max_map_count=262144",
+            local.start_service_container
+        ]
+    }
+}

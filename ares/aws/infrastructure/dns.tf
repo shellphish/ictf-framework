1330ddaed3a893e7f3736ac51f41ddada9ea5295
Sebastiano Mariani <mariani.sebastiano@gmail.com>
2020-05-22 12:48:35 -0700

    Fix duplicated log in logstash
    Signed-off-by: Sebastiano Mariani <mariani.sebastiano@gmail.com>

--- ares/aws/infrastructure/dns.tf
+++ ares/aws/infrastructure/dns.tf
@@ -21,3 +21,27 @@ resource "aws_route53_record" "logger" {
   ttl     = "43200" // 12h
   records = [aws_instance.logger.private_ip]
 }
+
+resource "aws_route53_record" "gamebot" {
+  zone_id = aws_route53_zone.private.zone_id
+  name    = "gamebot.ictf"
+  type    = "A"
+  ttl     = "43200" // 12h
+  records = [aws_instance.gamebot.private_ip]
+}
+
+resource "aws_route53_record" "scoreboard" {
+  zone_id = aws_route53_zone.private.zone_id
+  name    = "scoreboard.ictf"
+  type    = "A"
+  ttl     = "43200" // 12h
+  records = [aws_instance.scoreboard.private_ip]
+}
+
+resource "aws_route53_record" "teaminterface" {
+  zone_id = aws_route53_zone.private.zone_id
+  name    = "teaminterface.ictf"
+  type    = "A"
+  ttl     = "43200" // 12h
+  records = [aws_instance.teaminterface.private_ip]
+}

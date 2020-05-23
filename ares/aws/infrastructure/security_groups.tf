6a137eae52c0c06a005b377dd9574fcb195f4df9
Ubuntu <ubuntu@ip-172-31-121-58.us-west-1.compute.internal>
2020-05-23 05:28:32 +0000

    Add security rules and open ports for prometheus and grafana
    Signed-off-by: Ubuntu <ubuntu@ip-172-31-121-58.us-west-1.compute.internal>

--- ares/aws/infrastructure/security_groups.tf
+++ ares/aws/infrastructure/security_groups.tf
@@ -36,6 +36,14 @@ resource "aws_security_group" "master_subnet_secgrp" {
         cidr_blocks = ["0.0.0.0/0"]
     }
 
+    # Grafana
+    ingress {
+        from_port = 3000
+        to_port = 3000
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+
     # Elasticsearch
     ingress {
         from_port = 9200
@@ -44,11 +52,12 @@ resource "aws_security_group" "master_subnet_secgrp" {
         cidr_blocks = ["0.0.0.0/0"]
     }
 
-    ingress { # Prometheus Metrics
+    # Prometheus Metrics
+    ingress { 
         from_port   = 9000
         to_port     = 9999
         protocol    = "tcp"
-        cidr_blocks = ["192.35.222.0/24"]
+        cidr_blocks = ["0.0.0.0/0"]
     }
 
     egress {

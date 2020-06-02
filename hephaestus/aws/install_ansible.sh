e330a2c74b2a3a692d630fd45d12f56a781aa845
Sebastiano Mariani <mariani.sebastiano@gmail.com>
2020-06-02 15:42:55 -0700

    Add firt stage and second stage provisioninig folder in aws_ictf_base
    Signed-off-by: Sebastiano Mariani <mariani.sebastiano@gmail.com>

--- hephaestus/aws/install_ansible.sh
+++ hephaestus/aws/install_ansible.sh
@@ -1,16 +1,12 @@
 #!/bin/bash -eux
 
-# This sleep comes from the packer.io documentation
-# (https://www.packer.io/intro/getting-started/provision/)
-# sleep 30
-whoami
 # Install Ansible repository.
 # The line above will cause the process to get stuck during the updating of grub
 # See: https://github.com/chef/bento/issues/661#issuecomment-248136601
 DEBIAN_FRONTEND=noninteractive sudo apt-get -y -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" upgrade
-sudo apt -y install software-properties-common
+sudo apt-get -y install software-properties-common
 sudo apt-add-repository ppa:ansible/ansible
 
 # Install Ansible.
-sudo apt -y update
-sudo apt -y install ansible
+sudo apt-get -y update
+sudo apt-get -y install ansible
